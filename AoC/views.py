22e47a3c127003a1c9f7a0ce0164661408d24143
FredrikEglese <fredrik.eglese@gmail.com>
2020-01-31 15:31:27 +0000

    Passing content to index

--- AoC/views.py
+++ AoC/views.py
@@ -1,11 +1,14 @@
 from django.views import generic
+import os
 
 class IndexView(generic.ListView):
     template_name = 'AoC/index.html'
+    context_object_name = 'avaliable_solution_list'
 
     def get_queryset(self):
-        return True
-    
+        return (["a", "b"])
+        
+
 class SolveView(generic.DetailView):
     template_name = 'AoC/solve.html'
 
