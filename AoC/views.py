847ee462e9135cc68c3763e5944eff646846a152
FredrikEglese <fredrik.eglese@gmail.com>
2020-02-01 01:26:49 +0000

    Tidy up whitespace

--- AoC/views.py
+++ AoC/views.py
@@ -4,12 +4,12 @@ import os
 def index(request):
     context = {}
     solution_files_list = []
+
     for file_name in os.listdir("./AoC/solutions"):
         if file_name.endswith(".py"):
             solution_files_list.append(file_name[:3])
 
     solution_files_list.sort()
-
     context["solution_files_list"] = solution_files_list
 
     return render(request, "AoC/index.html", context)
@@ -17,13 +17,15 @@ def index(request):
 def solve(request, question_id):
     context = {}
     split_question_id = question_id.split("_")
+
     context["question_title"] = "Day " + split_question_id[0] + " - Part " + split_question_id[1]
     context["question_id"] = question_id
 
     return render(request, "AoC/solve.html", context)
 
 def result(request, question_id, user_input):
-    context = {}    
+    context = {}  
+      
     result_value = 12
     context["result_value"] = result_value
 
